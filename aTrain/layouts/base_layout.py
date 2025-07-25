5f281ed45c2571595cea776be58e38e1ccb6cddc
ArminHaberl <haberlarmin@gmx.at>
2025-07-25 14:41:14 +0200

    add sidebar to base layout

--- aTrain/layouts/base_layout.py
+++ aTrain/layouts/base_layout.py
@@ -2,11 +2,13 @@ from contextlib import contextmanager
 from nicegui import ui
 from aTrain.components.header import header
 from aTrain.components.footer import footer
+from aTrain.components.sidebar import sidebar
 
 
 @contextmanager
 def base_layout():
-    ui.query("body").classes("bg-gray-50")
+    ui.query("body").classes("bg-gray-100")
     header()
+    sidebar()
     yield
     footer()
