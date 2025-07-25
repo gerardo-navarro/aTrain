62cedfff37a6eef5202d52a11924d41f3cf8b62d
ArminHaberl <haberlarmin@gmx.at>
2025-07-25 11:20:37 +0200

    move header to its own component

--- aTrain/layouts/base_layout.py
+++ aTrain/layouts/base_layout.py
@@ -1,30 +1,10 @@
 from contextlib import contextmanager
 from nicegui import ui
-from importlib.resources import files
-from aTrain.version import __version__
-
-ATRAIN_LOGO = files("aTrain") / "static" / "images" / "logo.svg"
-GITHUB_LOGO = files("aTrain") / "static" / "images" / "github.svg"
-GITHUB_LINK = "https://github.com/JuergenFleiss/aTrain"
-
-
-def header():
-    with ui.header().classes("bg-white justify-between items-center px-10"):
-        with ui.row().classes("items-center"):
-            ui.button("☰")
-            ui.image(ATRAIN_LOGO).props("height='30px' width='80px' fit='contain'")
-        with ui.row().classes("items-center"):
-            ui.image(GITHUB_LOGO).props("height='25px' width='25px' fit='contain'")
-            with ui.link(target=GITHUB_LINK, new_tab=True).classes("text-black"):
-                ui.label(f"Version {__version__}")
-
-
-def theme():
-    ui.query("body").classes("bg-slate-200")
+from aTrain.components.header import header
 
 
 @contextmanager
 def base_layout():
-    theme()
+    ui.query("body").classes("bg-slate-200")
     header()
     yield
