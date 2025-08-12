f7d07c1dc6ed0d5c45a8b016e707372bf3be3065
ArminHaberl <haberlarmin@gmx.at>
2025-08-12 17:14:36 +0200

    remove redundant method

--- aTrain/components/file_picker.py
+++ aTrain/components/file_picker.py
@@ -2,9 +2,6 @@ from nicegui import ui
 
 
 class CustomUpload(ui.upload):
-    def remove_files(self):
-        self.run_method("removeQueuedFiles")
-
     def pick_files(self):
         self.run_method("pickFiles")
 
@@ -28,7 +25,7 @@ def file_picker() -> CustomUpload:
             div.classes("border border-rounded w-32 h-8 overflow-hidden")
             ui.label().props("outlined readonly").bind_text(data, "file_text")
 
-    select_button.on_click(uploader.remove_files)
+    select_button.on_click(uploader.reset)
     select_button.on_click(uploader.pick_files)
 
     return uploader
