b8dcd52e499d66a2211808db832691ac35f03cd7
ArminHaberl <haberlarmin@gmx.at>
2025-09-12 12:47:35 +0200

    remove unused files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./aTrain/templates/*.html", "./aTrain/templates/*/*.html", "./aTrain/templates/*/*/*.html", "./aTrain/static/JS/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
-
