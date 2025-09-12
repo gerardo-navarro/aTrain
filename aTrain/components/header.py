9ace1a6c1b177ac1fd2aa9b2b957647e13a32fa6
ArminHaberl <haberlarmin@gmx.at>
2025-09-12 12:43:46 +0200

    refactor component

--- aTrain/components/header.py
+++ aTrain/components/header.py
@@ -10,9 +10,9 @@ GITHUB_LINK = "https://github.com/JuergenFleiss/aTrain"
 def header(drawer_handle: ui.drawer):
     with ui.header().classes("bg-white justify-between items-center px-10"):
         with ui.row().classes("items-center"):
-            menu_button = ui.button().classes("lt-md")
-            menu_button.props("color=white text-color=black icon=menu flat")
-            menu_button.on_click(lambda: drawer_handle.toggle())
+            with ui.button().classes("lt-md") as menu_button:
+                menu_button.props("color=white text-color=black icon=menu flat")
+                menu_button.on_click(lambda: drawer_handle.toggle())
             ui.image(ATRAIN_LOGO).props("height=30px width=80px fit=contain")
         with ui.row().classes("items-center"):
             ui.image(GITHUB_LOGO).props("height=25px width=25px fit=contain")
