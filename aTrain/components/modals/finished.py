edace3087fd137cad2d12e1d1c9a17b935f83a9c
ArminHaberl <haberlarmin@gmx.at>
2025-09-15 19:20:14 +0200

    add timer

--- aTrain/components/modals/finished.py
+++ aTrain/components/modals/finished.py
@@ -1,7 +1,9 @@
-from nicegui import ui
+from nicegui import ui, app
 
 
 def modal_finished():
+    state = app.storage.client
     with ui.dialog(value=True).props("persistent") as dialog, ui.card():
         ui.label("Finished")
+        ui.label("").bind_text_from(state, "timer", lambda x: f"We transcribed in {x}")
         ui.button("Exit").on_click(dialog.delete)
--- aTrain/components/modals/process.py
+++ aTrain/components/modals/process.py
@@ -1,7 +1,16 @@
-from nicegui import ui
+from nicegui import ui, app
+from datetime import datetime
 
 
 def modal_process():
+    state = app.storage.client
+    start_time = datetime.now()
     with ui.dialog(value=True).props("persistent") as dialog, ui.card():
-        ui.label("Process")
+        ui.label("Process").classes("h2")
+        ui.separator()
+        ui.label("").bind_text(state, "timer")
+        ui.timer(
+            interval=1,
+            callback=lambda: state.update({"timer": str(datetime.now() - start_time)}),
+        )
     return dialog
