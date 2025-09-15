ec2def4b62dc0e360837af751baceccce3ec43ee
ArminHaberl <haberlarmin@gmx.at>
2025-09-15 19:51:30 +0200

    format timer and stop it after completion

--- aTrain/components/modals/process.py
+++ aTrain/components/modals/process.py
@@ -5,12 +5,18 @@ from datetime import datetime
 def modal_process():
     state = app.storage.client
     start_time = datetime.now()
+    timer = ui.timer(interval=1, callback=lambda: update_timer(start_time))
     with ui.dialog(value=True).props("persistent") as dialog, ui.card():
         ui.label("Process").classes("h2")
         ui.separator()
         ui.label("").bind_text(state, "timer")
-        ui.timer(
-            interval=1,
-            callback=lambda: state.update({"timer": str(datetime.now() - start_time)}),
-        )
-    return dialog
+    return dialog, timer
+
+
+def update_timer(start_time: datetime):
+    state = app.storage.client
+    timedelta = datetime.now() - start_time
+    total_seconds = int(timedelta.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    state["timer"] = f"{hours:02}:{minutes:02}:{seconds:02}"
--- aTrain/transcription.py
+++ aTrain/transcription.py
@@ -17,7 +17,7 @@ MultiPartParser.spool_max_size = FILE_SIZE_LIMIT
 
 
 async def start_transcription(file: events.UploadEventArguments):
-    process_modal = modal_process()
+    process_modal, timer = modal_process()
     _, file_id, timestamp = prepare_transcription(Path(file.name))
     state = app.storage.client
     try:
@@ -44,8 +44,10 @@ async def start_transcription(file: events.UploadEventArguments):
             required_models_dir=REQUIRED_MODELS_DIR,
         )
         process_modal.delete()
+        timer.cancel()
         modal_finished()
 
     except (SubprocessException, ValueError) as e:
         process_modal.delete()
+        timer.cancel()
         modal_error(error=str(e), traceback=traceback.format_exc())
