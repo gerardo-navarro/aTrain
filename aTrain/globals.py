691a382d37aa9760b9022ca17a6f899404bb4536
ArminHaberl <haberlarmin@gmx.at>
2025-09-15 17:51:43 +0200

    add file size limit to globals

--- aTrain/globals.py
+++ aTrain/globals.py
@@ -6,3 +6,4 @@ MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
 RUNNING_DOWNLOADS = []
 RUNNING_TRANSCRIPTIONS = []
 EVENT_SENDER = EventSender()
+FILE_SIZE_LIMIT = 1024 * 1024 * 1024 * 10  # 10 GB file size limit
