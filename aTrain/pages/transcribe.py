b0a49fecca8b3664387804f7d88b2a6fda78e26b
ArminHaberl <haberlarmin@gmx.at>
2025-09-16 12:11:44 +0200

    rename variable for clarity

--- aTrain/pages/transcribe.py
+++ aTrain/pages/transcribe.py
@@ -15,7 +15,7 @@ def page():
         with ui.element("div").classes(
             "w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"
         ):
-            uploader = input_file()
+            file = input_file()
             input_model()
             input_language()
             input_speaker_detection()
@@ -25,6 +25,6 @@ def page():
             with ui.column():
                 ui.label("Advanced Settings")
                 ui.link("Help needed?", "/faq")
-            ui.button("Start", on_click=uploader.upload)
+            ui.button("Start", on_click=file.upload)
 
-    uploader.on_upload(start_transcription)
+    file.on_upload(start_transcription)
