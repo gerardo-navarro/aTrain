423cc9051225a6d99b861cce3ab89917b0da2d3c
ArminHaberl <haberlarmin@gmx.at>
2025-09-16 11:41:55 +0200

    add shared dict for progress updates

--- aTrain/components/dialogs/process.py
+++ aTrain/components/dialogs/process.py
@@ -1,23 +1,35 @@
 from nicegui import ui, app
 from datetime import datetime
 from nicegui.run import tear_down as stop_transcription
+from multiprocessing.managers import DictProxy
 
 
-def dialog_process():
+def dialog_process(progress: DictProxy):
     global timer, dialog
     state = app.storage.client
     start_time = datetime.now()
-    timer = ui.timer(interval=1, callback=lambda: update_timer(start_time))
+    timer = ui.timer(interval=1, callback=lambda: update_progress(progress, start_time))
     with ui.dialog(value=True).props("persistent") as dialog, ui.card():
         ui.label("Process").classes("h2")
         ui.separator()
         ui.label("").bind_text(state, "timer")
+        ui.separator()
+        ui.label("").bind_text(state, "task")
+        ui.linear_progress(show_value=False).bind_value(state, "progress")
+        ui.separator()
         ui.button("stop").on_click(stop_transcription)
 
 
 def close_dialog_process():
     timer.cancel()
     dialog.delete()
+
+
+def update_progress(progress: DictProxy, start_time: datetime):
+    state = app.storage.client
+    state["progress"] = progress["current"] / progress["total"]
+    state["task"] = progress["task"]
+    update_timer(start_time)
 
 
 def update_timer(start_time: datetime):
--- aTrain/transcription.py
+++ aTrain/transcription.py
@@ -7,47 +7,49 @@ from aTrain_core.transcribe import prepare_transcription, transcribe
 from nicegui import app, events, run
 from nicegui.run import SubprocessException, setup as setup_process_pool
 from starlette.formparsers import MultiPartParser
-
+from multiprocessing import Manager
 from aTrain.components.dialogs.error import dialog_error
 from aTrain.components.dialogs.finished import dialog_finished
 from aTrain.components.dialogs.process import dialog_process, close_dialog_process
-from aTrain.globals import EVENT_SENDER, FILE_SIZE_LIMIT
+from aTrain.globals import FILE_SIZE_LIMIT
 
 MultiPartParser.spool_max_size = FILE_SIZE_LIMIT
 
 
 async def start_transcription(file: events.UploadEventArguments):
-    dialog_process()
-    _, file_id, timestamp = prepare_transcription(Path(file.name))
-    state = app.storage.client
-    try:
-        check_inputs_transcribe(
-            file=file.name,
-            model=state.get("model"),
-            language=state.get("language"),
-            device="cpu",
-        )
-        await run.cpu_bound(
-            transcribe,
-            audio_file=file.content,
-            file_id=file_id,
-            model=state.get("model"),
-            language=state.get("language"),
-            speaker_detection=state.get("speaker_detection"),
-            num_speakers=state.get("num_speakers") or "auto-detect",
-            device="cpu",  # fixed for testing
-            compute_type="int8",  # fixed for testing
-            timestamp=timestamp,
-            original_audio_filename=file.name,
-            initial_prompt=None,  # fixed for testing
-            GUI=EVENT_SENDER,
-            required_models_dir=REQUIRED_MODELS_DIR,
-        )
-        dialog_finished()
+    with Manager() as manager:
+        progress = manager.dict({"task": "Prepare", "current": 0, "total": 999999})
+        dialog_process(progress)
+        _, file_id, timestamp = prepare_transcription(Path(file.name))
+        state = app.storage.client
+        try:
+            check_inputs_transcribe(
+                file=file.name,
+                model=state.get("model"),
+                language=state.get("language"),
+                device="cpu",
+            )
+            await run.cpu_bound(
+                transcribe,
+                audio_file=file.content,
+                file_id=file_id,
+                model=state.get("model"),
+                language=state.get("language"),
+                speaker_detection=state.get("speaker_detection"),
+                num_speakers=state.get("num_speakers") or "auto-detect",
+                device="cpu",  # fixed for testing
+                compute_type="int8",  # fixed for testing
+                timestamp=timestamp,
+                original_audio_filename=file.name,
+                initial_prompt=None,  # fixed for testing
+                progress=progress,
+                required_models_dir=REQUIRED_MODELS_DIR,
+            )
+            dialog_finished()
 
-    except (SubprocessException, ValueError) as e:
-        dialog_error(error=str(e), traceback=traceback.format_exc())
+        except (SubprocessException, ValueError) as e:
+            dialog_error(error=str(e), traceback=traceback.format_exc())
 
-    except BrokenProcessPool:
-        close_dialog_process()
-        setup_process_pool()
+        except BrokenProcessPool:
+            close_dialog_process()
+            setup_process_pool()
