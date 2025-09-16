8f05d07802af383f46b5fc00d506d71e1477415b
ArminHaberl <haberlarmin@gmx.at>
2025-09-16 18:14:19 +0200

    add faq page

--- aTrain/pages/faq.py
+++ aTrain/pages/faq.py
@@ -1,8 +1,16 @@
 from nicegui import ui
-from aTrain.layouts.base_layout import base_layout
+from aTrain.layouts.card_layout import card_layout
+from aTrain.archive import load_faqs
 
 
 @ui.page("/faq")
 def page():
-    with base_layout():
-        ui.label("Lorem Ipsum")
+    faqs = load_faqs()
+    with card_layout():
+        ui.label("Frequently Asked Questions").classes("text-lg text-dark font-bold")
+        ui.separator()
+        with ui.column().classes("gap-3 w-full"):
+            for faq in faqs:
+                with ui.expansion(faq["question"], group="faq") as expansion:
+                    expansion.classes("w-full").props("dense")
+                    ui.label(faq["answer"]).classes("font-light")
