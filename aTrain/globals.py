0dd124e8c8653ec5f2ed0866a8e8e2fd09970ffd
ArminHaberl <haberlarmin@gmx.at>
2025-09-16 11:41:45 +0200

    remove event sender

--- aTrain/globals.py
+++ aTrain/globals.py
@@ -1,9 +1,8 @@
 import os
 from aTrain_core.globals import ATRAIN_DIR
-from aTrain_core.GUI_integration import EventSender
+
 
 MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
 RUNNING_DOWNLOADS = []
 RUNNING_TRANSCRIPTIONS = []
-EVENT_SENDER = EventSender()
 FILE_SIZE_LIMIT = 1024 * 1024 * 1024 * 10  # 10 GB file size limit
