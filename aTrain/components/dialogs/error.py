263a1d80784945e8ad381b1c9de1d969bf6267bd
ArminHaberl <haberlarmin@gmx.at>
2025-09-17 13:08:30 +0200

    reload page after error dialog to reset settings

--- aTrain/components/dialogs/error.py
+++ aTrain/components/dialogs/error.py
@@ -4,10 +4,10 @@ from aTrain.components.dialogs.process import close_dialog_process
 
 def dialog_error(error: str, traceback: str):
     close_dialog_process()
-    with ui.dialog(value=True).props("persistent") as dialog, ui.card():
+    with ui.dialog(value=True).props("persistent"), ui.card():
         ui.label("Error")
         ui.label(error)
         ui.label("Traceback")
         ui.label(traceback)
         btn_exit = ui.button("Exit", color="dark").props("unelevated no-caps")
-        btn_exit.on_click(dialog.delete)
+        btn_exit.on_click(ui.navigate.reload)
