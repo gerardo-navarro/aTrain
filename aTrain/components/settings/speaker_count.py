d914a14ed62b7092730b599a8e2333eb408b29dc
ArminHaberl <haberlarmin@gmx.at>
2026-01-14 11:26:45 +0100

    make automatic speaker count detection explicitly visible in the ui

--- aTrain/components/settings/speaker_count.py
+++ aTrain/components/settings/speaker_count.py
@@ -1,17 +1,21 @@
-from nicegui import ui, app
-
-TOOLTIP = "If you specify '0' the app will automatically detect the number of speakers."
+from nicegui import app, ui
+from nicegui.events import ValueChangeEventArguments
 
 
 def input_speaker_count():
     with ui.column().classes("gap-2") as column:
-        with ui.row(align_items="center").classes("w-full justify-between"):
-            ui.label("Number of Speakers").classes("font-bold text-dark text-md")
-            ui.icon("info_outline", size="sm", color="grey").tooltip(TOOLTIP)
+        ui.label("Number of Speakers").classes("font-bold text-dark text-md")
         ui.separator()
-        input = ui.number(min=0, value=0, placeholder="0")
+        input = ui.number(min=0, placeholder="Detect automatically")
         input.classes("w-full")
         input.props("filled bg-color=gray-100 color=dark")
 
+    input.on_value_change(lambda args: check_value(input, args))
     input.bind_value(app.storage.general, "speaker_count")
     column.bind_visibility(app.storage.general, "speaker_detection")
+
+
+def check_value(input: ui.number, args: ValueChangeEventArguments):
+    if args.value == 0.0:
+        input.set_value(None)
+        input.update()
