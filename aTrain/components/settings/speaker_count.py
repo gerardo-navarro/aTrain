a4869bdd1592f094260f85aad2f31e16bd73769b
ArminHaberl <haberlarmin@gmx.at>
2026-01-27 12:18:46 +0100

    make speaker count setting clearable

--- aTrain/components/settings/speaker_count.py
+++ aTrain/components/settings/speaker_count.py
@@ -1,21 +1,13 @@
 from nicegui import app, ui
-from nicegui.events import ValueChangeEventArguments
 
 
 def input_speaker_count():
     with ui.column().classes("gap-2") as column:
         ui.label("Number of Speakers").classes("font-bold text-dark text-md")
         ui.separator()
-        input = ui.number(min=0, placeholder="Detect automatically")
+        input = ui.number(min=1, placeholder="Detect automatically")
         input.classes("w-full")
-        input.props("filled bg-color=gray-100 color=dark")
+        input.props("filled bg-color=gray-100 color=dark clearable")
 
-    input.on_value_change(lambda args: check_value(input, args))
     input.bind_value(app.storage.general, "speaker_count")
     column.bind_visibility(app.storage.general, "speaker_detection")
-
-
-def check_value(input: ui.number, args: ValueChangeEventArguments):
-    if args.value == 0.0:
-        input.set_value(None)
-        input.update()
